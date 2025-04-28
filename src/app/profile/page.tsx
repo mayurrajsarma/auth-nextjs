26ee28c2e42842bc2f0e32d70ae50d847b600d12
mayur <mayurrajsarma202@gmail.com>
2025-04-28 12:34:19 +0530

    logout route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,24 @@
+'use client' ;
+import axios from 'axios' ;
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
 export default function ProfilePage() {
+    const router = useRouter() ;
+    const logout = async ()=> {
+        try {
+            await axios.get('/api/users/logout')
+            router.push('/login') ;
+
+        } catch (error: any) {
+            console.log(error.message);
+        }
+    }
     return (
-        <div className="flex justify-center items-center min-h-screen">
+        <div className="flex flex-col justify-center items-center min-h-screen">
             <h1>Profile Page</h1>
+            <hr/>
+            <button onClick={logout} className="p-2 border hover:bg-blue-300 mt-2">Logout</button>
         </div>
     )
 }
