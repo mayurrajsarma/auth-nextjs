524c3a6096aacf1b058e88b2eaf3cb93694ea72f
mayur <mayurrajsarma202@gmail.com>
2025-04-29 09:36:11 +0530

    middleware and me route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,9 +2,11 @@
 import axios from 'axios' ;
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export default function ProfilePage() {
     const router = useRouter() ;
+    const [data,setData] = useState("nothing") ;
     const logout = async ()=> {
         try {
             await axios.get('/api/users/logout')
@@ -14,11 +16,22 @@ export default function ProfilePage() {
             console.log(error.message);
         }
     }
+
+    const getUserDetails = async ()=> {
+        const res = await axios.get('/api/users/me') ;
+        // console.log(res.data) ;
+        setData(res.data.user._id) ;
+        console.log(data);
+    }
+
     return (
         <div className="flex flex-col justify-center items-center min-h-screen">
             <h1>Profile Page</h1>
             <hr/>
+            <h2 className='bg-gray-400 p-2'>{data === "nothing" ? "Nothing" : <Link href={`/profile/${data}`}>{data}</Link>}</h2>
+            <hr/>
             <button onClick={logout} className="p-2 border hover:bg-blue-300 mt-2">Logout</button>
+            <button onClick={getUserDetails} className="p-2 border mt-2">User Data</button>
         </div>
     )
 }
